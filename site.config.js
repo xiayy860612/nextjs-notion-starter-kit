72612b904b3721ab4c2e0241522556bda5e526af
Amos Xia <xiayy860612@126.com>
2021-11-10 22:39:39 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '45e3f70d1be7425ab41f3c549ddce779',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '观澜',
+  domain: 'amos.s2u2m.com',
+  author: 'Amos Xia',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,8 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: '',
+  github: 'xiayy860612',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
